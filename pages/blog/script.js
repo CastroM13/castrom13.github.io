2a2e21cfb6f91a2d8e4a7f864ebf861a52c88550
Matheus Castro <matheus.castro@ciag.org.br>
2023-04-20 08:16:24 -0300

    feat: new slider

--- pages/blog/script.js
+++ pages/blog/script.js
@@ -2,16 +2,22 @@ const slider = document.querySelector('.slider');
 const btnLeft = document.querySelector('.left');
 const btnRight = document.querySelector('.right');
 
-btnLeft.addEventListener('click', () => {
-  slider.scrollBy({
-    left: -slider.offsetWidth,
-    behavior: 'smooth'
-  });
+btnLeft.addEventListener('click', (event) => {
+  if (event.detail == 1) {
+    slider.scrollBy({
+      left: -slider.offsetWidth,
+      behavior: 'smooth'
+    });
+  }
 });
 
-btnRight.addEventListener('click', () => {
-  slider.scrollBy({
-    left: slider.offsetWidth,
-    behavior: 'smooth'
-  });
+btnRight.addEventListener('click', (event) => {
+  if (event.detail == 1) {
+    slider.scrollBy({
+      left: slider.offsetWidth,
+      behavior: 'smooth'
+    });
+  }
 });
+
+console.log(slider.querySelectorAll(".slide"))
