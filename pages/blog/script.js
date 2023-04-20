d35851d813c71316cbff529b048a95727077e4c4
Matheus Castro <matheus.castro@ciag.org.br>
2023-04-20 16:13:31 -0300

    feat: added blog posts

--- pages/blog/script.js
+++ pages/blog/script.js
@@ -1,7 +1,21 @@
-onload = () => {
-  
+const renderPost = (event) => {
+  const url = event.target.href;
+  fetch(url)
+    .then(e => e.text())
+    .then(e => console.log(e))
 }
 
+fetch('https://api.github.com/repos/CastroM13/CastroM13/git/trees/ac51b3e76346df1371206226faa3020c0a740272')
+  .then(e => e.json())
+  .then(e => e.tree.forEach(post => {
+    const link = document.createElement('p')
+    link.href = 'https://raw.githubusercontent.com/CastroM13/CastroM13/main/blog/' + encodeURI(post.path);
+    link.onclick = renderPost;
+    link.textContent = post.path.split('.md')[0];
+    console.log(link)
+    document.querySelector('#blog-wrapper').appendChild(link)
+  }));
+ß
 const slider = document.querySelector('.slider');
 const btnLeft = document.querySelector('.left');
 const btnRight = document.querySelector('.right');
@@ -11,11 +25,11 @@ let sliderIndex = 0;
 const updatePagerButton = (sliderIndex) => {
   const active = pager.querySelector('.active');
   active && active.classList.remove('active');
-  pager.querySelector('#slide-'+sliderIndex).classList.add('active')
+  pager.querySelector('#slide-' + sliderIndex).classList.add('active')
 };
 
 const slide = (times = 0) => {
-  const slidesSize =  (slider.querySelectorAll(".slide").length - 1);
+  const slidesSize = (slider.querySelectorAll(".slide").length - 1);
   const higherThanZero = (sliderIndex + times) >= 0;
   const smallerThanSize = (sliderIndex + times) <= slidesSize;
   if (higherThanZero && smallerThanSize) {
@@ -46,7 +60,7 @@ const slideTo = (event) => {
   if (i === 0) {
     pagerButton.className = 'active';
   }
-  pagerButton.id = 'slide-'+i;
+  pagerButton.id = 'slide-' + i;
   pagerButton.onclick = slideTo;
   pager.appendChild(pagerButton);
 });
